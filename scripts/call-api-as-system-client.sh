40e5ebd9cca75cc6a2c5ed4423c281f86508a04e
james-baker-moj <159048573+james-baker-moj@users.noreply.github.com>
2024-08-22 13:43:28 +0100

    PRC-52: Script to get token to use with swagger (#14)
    Split up the utility scripts so there's one just for getting the auth token which can be used with the swagger docs.

--- scripts/call-api-as-system-client.sh
+++ scripts/call-api-as-system-client.sh
@@ -12,19 +12,8 @@
 # $ ./call-api-as-system-client.sh <URL>
 #
 
-AUTH_HOST="https://sign-in-dev.hmpps.service.justice.gov.uk"
-
-read -r user secret < <(echo $(kubectl -n hmpps-contacts-dev get secret hmpps-contacts-ui -o json | jq '.data[] |= @base64d' | jq -r '.data.SYSTEM_CLIENT_ID, .data.SYSTEM_CLIENT_SECRET'))
-
-BASIC_AUTH="$(echo -n $user:$secret | base64)"
-TOKEN_RESPONSE=$(curl -s -k -d "" -X POST "$AUTH_HOST/auth/oauth/token?grant_type=client_credentials" -H "Authorization: Basic $BASIC_AUTH")
-TOKEN=$(echo "$TOKEN_RESPONSE" | jq -er .access_token)
-if [[ $? -ne 0 ]]; then
-  echo "Failed to read token from credentials response"
-  echo "$TOKEN_RESPONSE"
-  exit 1
-fi
-
+DIR=$(dirname "$0")
+TOKEN=$(source "$DIR/get-auth-token.sh")
 curl -s -X GET --location "$1" -H "Authorization: Bearer $TOKEN" -H "Accept: application/json" | jq .
 
 # End

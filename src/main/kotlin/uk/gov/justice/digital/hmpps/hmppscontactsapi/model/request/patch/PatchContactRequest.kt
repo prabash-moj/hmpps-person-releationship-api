02eeaa4aab1b735072d568c8ede6b07bed151834
james-baker-moj <159048573+james-baker-moj@users.noreply.github.com>
2024-10-28 13:31:27 +0000

    PRC-73: Fix required-ness in swagger for patch request (#95)
    Only updatedBy is actually required

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/model/request/patch/PatchContactRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/model/request/patch/PatchContactRequest.kt
@@ -3,21 +3,21 @@ package uk.gov.justice.digital.hmpps.hmppscontactsapi.model.request.patch
 import io.swagger.v3.oas.annotations.media.Schema
 import org.openapitools.jackson.nullable.JsonNullable
 
-@Schema(description = "Request to patch a new contact ", nullable = true)
+@Schema(description = "Request to patch a new contact ")
 data class PatchContactRequest(
 
-  @Schema(description = "Whether the contact is a staff member", example = "false", nullable = true, type = "boolean")
+  @Schema(description = "Whether the contact is a staff member", example = "false", nullable = false, type = "boolean", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
   var staffFlag: JsonNullable<Boolean> = JsonNullable.undefined(),
 
-  @Schema(description = "The domestic status code of the contact", example = "S", nullable = true, type = "string")
+  @Schema(description = "The domestic status code of the contact", example = "S", nullable = true, type = "string", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
   var domesticStatus: JsonNullable<String?> = JsonNullable.undefined(),
 
-  @Schema(description = "Whether an interpreter is required", example = "false", nullable = true, type = "boolean")
+  @Schema(description = "Whether an interpreter is required", example = "false", nullable = false, type = "boolean", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
   var interpreterRequired: JsonNullable<Boolean> = JsonNullable.undefined(),
 
-  @Schema(description = "The language code of the contact", example = "EN", nullable = true, type = "string")
+  @Schema(description = "The language code of the contact", example = "EN", nullable = true, type = "string", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
   var languageCode: JsonNullable<String?> = JsonNullable.undefined(),
 
-  @Schema(description = "The id of the user who updated the contact", example = "JD000001", nullable = true)
+  @Schema(description = "The id of the user who updated the contact", example = "JD000001", nullable = false, requiredMode = Schema.RequiredMode.REQUIRED)
   val updatedBy: String,
 )

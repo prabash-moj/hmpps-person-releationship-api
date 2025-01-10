e0b5dce70b0c63e6072f653d5991bfb0d2b32cba
james-baker-moj <159048573+james-baker-moj@users.noreply.github.com>
2025-01-10 12:42:06 +0000

    PRC-402: Fix remove estimated DOB migration (#212)

--- src/main/resources/migrations/common/V2025.01.09.6__remove_estimated_dob.sql
+++ src/main/resources/migrations/common/V2025.01.09.6__remove_estimated_dob.sql
@@ -1,6 +1,10 @@
 --
--- Removing estimated DOB as this is not required or something that exists in NOMIS
+-- Removing estimated DOB as this is not required or something that exists in NOMIS.
+-- Dropping dependent views as their migrations will be re-run after versioned migrations.
 --
+
+DROP VIEW IF EXISTS v_prisoner_contacts;
+DROP VIEW IF EXISTS v_contacts_with_primary_address;
 
 ALTER TABLE contact DROP COLUMN estimated_is_over_eighteen;
 
